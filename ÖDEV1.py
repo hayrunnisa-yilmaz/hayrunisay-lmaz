0f0988983ba39b6b257e2f0b5487f5ec3acb580e
hayrunnisa0830-commits <hayrunnisa0830@gmail.com>
2026-04-27 19:51:30 +0300

    Add files via upload

---
+++ ÖDEV1.py
@@ -0,0 +1,18 @@
+print("KİSİSEL BİLGİLER")
+
+Ad = input("Adınızı giriniz: ")
+Soyad = input("Soyadınızı giriniz: ")
+Yas = input("Yasınızı giriniz: ")
+Sehir = input("Sehirinizi giriniz: ")
+Meslek = input("Mesleginizi giriniz: ")
+
+bes_yil_sonra_yas = yas + 5
+harf_sayisi = len(ad + soyad)   
+
+print(f"Ad Soyad : {ad} {soyad}")
+print(f"Yaş      : {yas}")
+print(f"Şehir    : {sehir}")
+print(f"Meslek   : {meslek}")
+
+print(f"5 yil sonraki yasiniz: {bes_yil_sonra_yas}")
+print(f"Ad ve soyad toplam harf sayisi: {harf_sayisi}")
